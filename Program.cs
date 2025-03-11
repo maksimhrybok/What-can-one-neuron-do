e3e76045c20f0cf9a554814a88d706e0c3d40087
maksimhrybok <maxgribok1204@gmail.com>
2025-03-11 03:08:18 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_can_one_neuron_do
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
